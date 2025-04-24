fd7d99fe23e17bc7561cb697d9bf0a752f09a4b0
1VIDIX <juan322mi@gmail.com>
2025-04-24 10:55:29 -0500

    a

--- app/src/main/java/com/example/taller/MainActivity.kt
+++ app/src/main/java/com/example/taller/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.taller
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val boton: Button = findViewById(R.id.boton_init)
+        boton.setOnClickListener {
+            val intent = Intent(this, iniciar_sesion::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
